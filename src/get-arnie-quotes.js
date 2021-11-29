ccf64b70d5a496bf0a6d209784baef7d9f1d9151
rlor3 <88142582+rlor3@users.noreply.github.com>
2021-11-30 10:47:15 +1100

    implemented getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,23 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
+  const results = await Promise.all(
+    urls.map(async (url) => {
+      const response = await httpGet(url);
+      if (response.status === 200) {
+        return {
+          "Arnie Quote": JSON.parse(response.body).message,
+        };
+      } else {
+        return {
+          FAILURE: JSON.parse(response.body).message,
+        };
+      }
+    })
+  );
+  return results;
 };
 
 module.exports = {
